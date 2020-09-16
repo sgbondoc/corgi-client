f6af32f24a18a26c4e71834ecf24b3238e63d211
sbgarcia <sharonbondoc.g@gmail.com>
2020-09-16 09:15:13 -0700

    added basic styling to create post page

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -6,36 +6,43 @@ const CreatePost = () => {
     const [image, setImage] = useState('')
 
     return (
-        <div className="create-post-card">
-            <input 
-                type="text"
-                name="title"
-                placeholder="title"
-                onChange={(e)=>setTitle(e.target.value)}
-            />
+        <div>
+            <div className="create-post-card">
+                <h4>Create Post</h4>
+                <form>
+                    <div>
+                        <input 
+                            type="text"
+                            name="title"
+                            placeholder="title"
+                            onChange={(e)=>setTitle(e.target.value)}
+                        />
 
-            <input 
-                type="text"
-                name="caption"
-                placeholder="caption"
-                onChange={(e)=>setBody(e.target.value)}
-            />
+                        <input 
+                            type="text"
+                            name="caption"
+                            placeholder="caption"
+                            onChange={(e)=>setBody(e.target.value)}
+                        />
 
-            <div className="file-field input-field">
-            <div className="btn">
-                <span>Add Image</span>
-                <input type="file" onChange={(e) => setImage(e.target.files[0])}/>
+                        <div className="file-field input-field">
+                        <div className="btn">
+                            <span>Add Image</span>
+                            <input type="file" onChange={(e) => setImage(e.target.files[0])}/>
+                        </div>
+                        <div className="file-path-wrapper">
+                            <input className="file-path validate" type="text"/>
+                        </div>
+                        </div>
+                        <button 
+                            class="waves-effect waves-light btn"
+                            type="submit">
+                            Submit
+                        </button>
+                    </div>    
+                </form>
             </div>
-            <div className="file-path-wrapper">
-                <input className="file-path validate" type="text"/>
-            </div>
-            </div>
-            <button 
-                class="waves-effect waves-light btn"
-                type="submit">
-                Submit
-            </button>
-        </div>
+        </div>    
     )
 }
 
