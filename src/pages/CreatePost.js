9d3f9f3bff4052c2449d4fe6f8c992e5c8f15f5f
sbgarcia <sharonbondoc.g@gmail.com>
2020-09-17 01:48:31 -0700

    working on connecting cloudinary - need to debug

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,46 +1,88 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router'
+import '../App.css'
 
 const CreatePost = () => {
     const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
+    const [caption, setCaption] = useState('')
     const [image, setImage] = useState('')
+    const [url, setUrl] = useState('')
+
+    const history = useHistory()
+
+    const handleSubmit = () => {
+        const data = new FormData()
+        data.append('file', image)
+        data.append('upload_preset', 'corgi-connect')
+        data.append('cloud_name', 'sbgarcia')
+        fetch('https://api.cloudinary.com/v1_1/sbgarcia/image/upload', {
+            method: "POST",
+            body: data
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setUrl(data.url)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        fetch('/createpost', {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({
+                title,
+                caption,
+                url
+            })    
+        }).then(response => response.json())
+        .then(data => {
+            console.log(data)
+            history.push('/')
+        }).catch(err => {
+            console.log(err)
+        })
+    }
 
     return (
         <div>
             <div className="create-post-card">
                 <h4>Create Post</h4>
-                <form>
                     <div>
                         <input 
                             type="text"
                             name="title"
                             placeholder="title"
-                            onChange={(e)=>setTitle(e.target.value)}
+                            value={ title }
+                            onChange={(event) => setTitle(event.target.value)}
                         />
 
                         <input 
                             type="text"
                             name="caption"
                             placeholder="caption"
-                            onChange={(e)=>setBody(e.target.value)}
+                            value={ caption }
+                            onChange={(event) => setCaption(event.target.value)}
                         />
 
                         <div className="file-field input-field">
                         <div className="btn">
                             <span>Add Image</span>
-                            <input type="file" onChange={(e) => setImage(e.target.files[0])}/>
+                            <input type="file" onChange={(event) => setImage(event.target.files[0])}/>
                         </div>
                         <div className="file-path-wrapper">
                             <input className="file-path validate" type="text"/>
                         </div>
                         </div>
+
                         <button 
-                            class="waves-effect waves-light btn"
-                            type="submit">
+                            className="waves-effect waves-light btn"
+                            type="submit"
+                            onClick={() => handleSubmit()}
+                            >
                             Submit
                         </button>
                     </div>    
-                </form>
             </div>
         </div>    
     )
