dd0f0e8365246f2977aefb353e7c72431124e072
sbgarcia <sharonbondoc.g@gmail.com>
2020-09-17 08:56:56 -0700

    installed reactstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,8 +19,8 @@ const NavBar = (props) => {
                         </>    
                     :            
                         <>
-                        <li><Link to="/register">Register</Link></li>
-                        <li><Link to="/login">Login</Link></li>
+                            <li><Link to="/register">Register</Link></li>
+                            <li><Link to="/login">Login</Link></li>
                         </>
                     }  
                     
