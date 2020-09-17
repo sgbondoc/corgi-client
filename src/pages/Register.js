b754f170bc8d4c8c3e059858610fabf1d61750f2
sbgarcia <sharonbondoc.g@gmail.com>
2020-09-17 00:28:13 -0700

    refactored register with useState

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,82 +1,76 @@
-import React, { Component } from 'react'
-import UserModel from '../models/user'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router'
 import '../App.css'
 
-class Register extends Component {
-    state = {
-        name: '',
-        email: '',
-        password: ''
-    }
+const Register = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const history = useHistory()
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault()
-        UserModel.create(this.state)
+    const handleSubmit = () => {
+        fetch('/register', {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            })    
+        }).then(response => response.json())
         .then(data => {
             console.log(data)
-            this.setState({
-                name: '',
-                email: '',
-                password: ''
-            })
-            this.props.history.push('/login')
+            history.push('/login')
+        }).catch(err => {
+            console.log(err)
         })
-        .catch(err => console.log(err))
     }
-    
-    render() {
-        return (
-            <div>
-                <div className="card">
-                    <div className="register-card">
-                    <h4>Register</h4>
-                        <form onSubmit={ this.handleSubmit }>
-                            <div className="register-form-card">
-                                
-                                <input
-                                    onChange={ this.handleChange }
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    placeholder="username"
-                                    value={ this.state.name }
-                                />
-            
-                                <input
-                                    onChange={ this.handleChange }
-                                    type="text"
-                                    id="email"
-                                    name="email"
-                                    placeholder="email"
-                                    value={ this.state.email }
-                                />
 
-                                <input
-                                    onChange={ this.handleChange }
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    placeholder="password"
-                                    value={ this.state.password }
-                                />
-                                <button 
-                                    className="waves-effect waves-light btn"
-                                    type="submit">
-                                    Register
-                                </button>
-                            </div>
-                        </form>
-                    </div>  
-                </div>          
-            </div>
-        )
-    }    
+    return (
+        <div>
+            <div className="card">
+                <div className="register-card">
+                    <h4>Register</h4>
+                        <div className="register-form-card">
+                            
+                            <input
+                                onChange={(event) => setName(event.target.value)}
+                                type="text"
+                                id="name"
+                                name="name"
+                                placeholder="username"
+                                value={ name }
+                            />
+        
+                            <input
+                                onChange={(event) => setEmail(event.target.value)}
+                                type="text"
+                                id="email"
+                                name="email"
+                                placeholder="email"
+                                value={ email }
+                            />
+
+                            <input
+                                onChange={(event) => setPassword(event.target.value)}
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="password"
+                                value={ password }
+                            />
+                            <button 
+                                className="waves-effect waves-light btn"
+                                onClick={() => handleSubmit()}
+                                >
+                                Register
+                            </button>
+                        </div>
+                </div>  
+            </div>          
+        </div>
+    )
 }
 
 export default Register
