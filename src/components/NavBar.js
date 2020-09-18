7cac2885f6d65f14c9383bc4e4b5ec5a9125721f
sbgarcia <sharonbondoc.g@gmail.com>
2020-09-18 02:37:17 -0700

    hiding and showing routes based on user data

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { UserContext } from '../App'
 
 
 const NavBar = (props) => {
+    const {state, dispatch} = useContext(UserContext)
+    const renderList =  () => {
+        // state now has details of the user (id, name, email)
+        if (state) {
+            return [
+                <li><Link to="/mygallery">My Gallery</Link></li>,
+                <li><Link to="/createpost">Create Post</Link></li>,
+                <li><Link to="/posts">All Posts</Link></li>,
+                <li><Link to="/chat">Chat</Link></li>,
+                <li><Link to="/logout">Logout</Link></li>
+            ]
+        } else {
+            return [
+                <li><Link to="/register">Register</Link></li>,
+                <li><Link to="/login">Login</Link></li>
+            ]    
+        }
+    }
+
     return (
         <nav>
             <div className="nav-wrapper #eceff1 blue-grey lighten-5">
                 <Link to="/" className="brand-logo left">CORGI CONNECT</Link>
                 <ul id="nav-mobile" className="right">
-                    
-                    { props.currentUser ?
-                        <>
-                            <li><Link to="/mygallery">My Gallery</Link></li>
-                            <li><Link to="/createpost">Create Post</Link></li>
-                            <li><Link to="/posts">All Posts</Link></li>
-                            <li><Link to="/chat">Chat</Link></li>
-                            <li><Link to="/logout" onClick={ props.logout }>Logout</Link></li>
-                        </>    
-                    :            
-                        <>
-                            <li><Link to="/register">Register</Link></li>
-                            <li><Link to="/login">Login</Link></li>
-                        </>
-                    }  
-                    
+                    { renderList() }            
                 </ul>
             </div>
         </nav>
