7c4653676c9f6c20b5d2eb52a4ae1a95b9e1644f
sbgarcia <sharonbondoc.g@gmail.com>
2020-09-20 05:14:29 -0700

    updated user model

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+export default class UserModel {
+    static async create(data) {
+        const response = await fetch('/register', {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        })
+        return await response.json()
+    }
+
+    static async login(email, password) {
+        const response = await fetch('/login', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                email,
+                password
+            })    
+         })
+         return await response.json()
+    }
+
+    static async logout() {
+        const response = await fetch('/logout', {
+                method: "DELETE",
+                credentials: "include",
+        })
+        return await response.json()
+    }
+   
+}
